79eb3ea107581a5a5c415aed139756d8e48c8dae
shofiyah1 <98735760+shofiyah1@users.noreply.github.com>
2022-08-31 20:37:14 +0700

    Delete lesson2_gettinginp.js

--- lesson2_gettinginp.js
+++
@@ -1,9 +0,0 @@
-const command = process.argv[2]
-
-console.log(process.argv)
-
-if (command === 'add') {
- console.log('Adding note!')
-} else if (command === 'remove') {
- console.log('Removing note!')
-}
